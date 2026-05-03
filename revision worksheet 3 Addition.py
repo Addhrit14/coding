e642ee7ff82eec5533f22b41a50db6baf2702f4b
Addhrit14 <25addhritbhushan.acc@lwetb.ie>
2026-05-03 13:29:05 +0100

    Add files via upload

---
+++ revision worksheet 3 Addition.py
@@ -0,0 +1,6 @@
+num1 = input('enter a number:')
+num1 = int(num1)
+num2 = input('enter a number:')
+num2 = int(num2)
+add = num1 + num2
+print (add)
---
+++ revision worksheet 3 Multiplication.py
@@ -0,0 +1,6 @@
+num1 = input('enter a number:')
+num1 = int(num1)
+num2 = input('enter a number:')
+num2 = int(num2)
+multiply = num1 * num2
+print (multiply)
